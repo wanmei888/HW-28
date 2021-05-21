4a6bba4c734cca9d4b62015e23652fb1dd23478b
Seiova Aigerim <aikawanmei@gmail.com>
2021-05-20 20:25:58 -1200

    Miukid project

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict"
+let ttl = document.getElementsByClassName("ttl");
+ let ul_li = document.getElementsByClassName("ul_li");
+console.log(ttl[1].innerText);
+console.log(ul_li[1].innerText);
+ 
